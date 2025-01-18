ab2235866119643627ba7409684a18388512fcf6
Olya Dubodel <dubodel.olya@gmail.com>
2025-01-19 02:37:59 +0300

    feat: solve the third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const { stdout } = require("process");
+
+fs.readdir(
+  path.join(__dirname, "secret-folder"),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach(file => {
+        if (file.isFile()) {
+          fs.stat(
+            path.join(__dirname, "secret-folder", file.name),
+            (err, stats) => {
+              if (err) {
+                console.log(err);
+              }
+              stdout.write(`${file.name.split('.')[0]} - ${path.extname(file.name).split('.')[1]} - ${stats.size}\n`);
+            }
+          );
+        }
+      })
+    }
+  }
+);
